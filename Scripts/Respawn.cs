313df28cd1f868cef2e63991cb87cf3f9794f215
francyleo <41713646+francyleo@users.noreply.github.com>
2018-10-07 06:01:40 -0300

    1

--- Scripts/Respawn.cs
+++ Scripts/Respawn.cs
@@ -1,4 +1,4 @@
-﻿/* using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -56,4 +56,3 @@ public class Respawn : MonoBehaviour
 		}
 	}
 }
- */
