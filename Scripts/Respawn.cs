2cb3916ad9e1a796be1df227728f2a55af444d53
francyleo <41713646+francyleo@users.noreply.github.com>
2018-10-07 14:02:03 -0300

    Termino do Desafio

--- Scripts/Respawn.cs
+++ Scripts/Respawn.cs
@@ -27,20 +27,32 @@ public class Respawn : MonoBehaviour
 	private InputField rotY;
 	[SerializeField]
 	private InputField rotZ;
+	//armazena o valor do numero usado para calcular o valor da distância.
+	private float aces;
 	//####### Variaveis #######//
+
+	void Start ()
+	{
+		aces = 130f;
+	}
 
 	//Função para instanciar clone do personagem na tela(Prefab)
 	public void create()
 	{
-		//pegar posicionamento e rotacionamento digitado
-		resp.position = new Vector3(float.Parse(posX.text),float.Parse(posY.text),float.Parse(posZ.text));
+		//pegar posicionamento digitado
+		resp.position = new Vector3(float.Parse(posX.text)*aces,float.Parse(posY.text)*aces,float.Parse(posZ.text)*aces);
 		//verificar se o personagem ja esta na tela
 		if(!isCreate)
 		{
 			clone = Instantiate(personagem,resp.position,Quaternion.identity);
-			clone.transform.Rotate (float.Parse(rotX.text),float.Parse(rotY.text),float.Parse(rotZ.text),Space.Self);
+			//designa a rotação do personagem
+			clone.transform.Rotate (float.Parse(rotX.text)*aces,float.Parse(rotY.text)*aces,float.Parse(rotZ.text)*aces,Space.Self);
 			isCreate = true;//para não criar mais de 1
 		}else {
+			/**
+				caso o personagem ja temha sido criado,deleta-o e cria um personagem
+				novo de acordo com as coordenadas atualizadas.	
+			 */
 			delete();
 			create();
 		}
